581b8cd6d2f616398823a5c3cd7da38225a8065c
FND <fnd@localhost.localdomain>
2018-01-24 19:55:22 +0100

    upgraded to latest faucet-core with centralized manifest
    in the process, adjusted tests and metadata to align with faucet-js

--- samples/faucet.config.js
+++ samples/faucet.config.js
@@ -1,18 +1,12 @@
-let config = {
-	manifest: {
-		file: "dist/manifest.json",
-		baseURI: "/assets"
-	},
-	bundles: [{
-		entryPoint: "index.jsx",
-		target: "dist/bundle.js",
+"use strict";
+
+module.exports = {
+	js: [{
+		source: "./index.jsx",
+		target: "./dist/bundle.js",
 		transpiler: {
 			features: ["es2015", "jsx"],
 			jsx: { pragma: "createElement" }
 		}
 	}]
-};
-
-module.exports = {
-	js: config
 };
--- test/test_transpilation/faucet.config.js
+++ test/test_transpilation/faucet.config.js
@@ -1,17 +1,12 @@
 "use strict";
 
-let config = {
-	manifest: false,
-	bundles: [{
-		entryPoint: "src/index.jsx",
-		target: "dist/bundle.js",
+module.exports = {
+	js: [{
+		source: "./src/index.jsx",
+		target: "./dist/bundle.js",
 		transpiler: {
 			features: ["es2015", "jsx"],
 			jsx: { pragma: "createElement" }
 		}
 	}]
-};
-
-module.exports = {
-	js: config
 };
